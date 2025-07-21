f030a8cf1d89649458b9212af3a20295f32b595c
Leane Brunelle <leanebrunelle2@gmail.com>
2025-07-21 17:14:26 -0300

    (ADD) Exercise 3

--- exercises.py
+++ exercises.py
@@ -19,7 +19,15 @@
 
 # print(f"The value of division is {division_total} and the remainder is {remainder} \n")
 
-# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+# 3. Develop a program that multiplies two numbers provided by the user and displays the result.
+# (Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.)
+
+number_1 = int(input("Type a number: "))
+number_2 = int(input("Type another number: "))
+multiplication = number_1 * number_2
+
+print(f"The value of multiplication is {multiplication}.")
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
