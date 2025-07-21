595da1116af89fef3bfa77b303dd16f6c1bd4622
Leane Brunelle <leanebrunelle2@gmail.com>
2025-07-21 18:42:13 -0300

    Full integers exercises

--- integers_exercises.py
+++ integers_exercises.py
@@ -1,32 +1,32 @@
-# #### Inteiros (`int`)
+# #### Integers (`int`)
 
 # 1. Write a program that adds two integers entered by the user. 
 # (Escreva um programa que soma dois números inteiros inseridos pelo usuário.)
 
-# number_one = int(input("Type a number: "))
-#number_two = int(input("Type another number: "))
-# number_total = number_one + number_two
+number_one = int(input("Type a number: "))
+number_two = int(input("Type another number: "))
+number_total = number_one + number_two
 
-# print(f"The total value is: {number_total} \n")
+print(f"The total value is: {number_total} \n")
 
 # 2. Create a program that receives a number from the user and calculates the remainder of dividing that number by 5.
 # (Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.)
 
-# divider = int(input("Type a number: "))
-# dividend = 5
-# division_total = divider / dividend
-# remainder = divider % dividend
+divider = int(input("Type a number: "))
+dividend = 5
+division_total = divider / dividend
+remainder = divider % dividend
 
-# print(f"The value of division is {division_total} and the remainder is {remainder} \n")
+print(f"The value of division is {division_total} and the remainder is {remainder}. \n")
 
 # 3. Develop a program that multiplies two numbers provided by the user and displays the result.
 # (Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.)
 
-# number_1 = int(input("Type a number: "))
-# number_2 = int(input("Type another number: "))
-# multiplication = number_1 * number_2
+number_1 = int(input("Type a number: "))
+number_2 = int(input("Type another number: "))
+multiplication = number_1 * number_2
 
-# print(f"The value of multiplication is {multiplication}.")
+print(f"The value of multiplication is {multiplication}. \n")
 
 # 4. Write a program that asks for two integers and prints the integer division of the first by the second.
 # (Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.)
@@ -35,38 +35,12 @@ divisor_1 = int(input("Type a number: "))
 dividend_1 = int(input("Typer another number: "))
 division_1 = divisor_1 // dividend_1
 
-print(f"The integer division is {division_1}")
+print(f"The integer division is {division_1}. \n")
 
-# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+# 5. Write a program that calculates the square of a number provided by the user.
+# (Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.)
 
-# #### Números de Ponto Flutuante (`float`)
+provide_number = int(input("Type a number: "))
+square_number = provide_number ** 2
 
-# 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
-# 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
-# 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
-# 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
-# 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
-
-# #### Strings (`str`)
-
-# 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
-# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
-# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
-# 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
-# 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
-
-# #### Booleanos (`bool`)
-
-# 16. Escreva um programa que avalie duas expressões booleanas inseridas pelo usuário e retorne o resultado da operação AND entre elas.
-# 17. Crie um programa que receba dois valores booleanos do usuário e retorne o resultado da operação OR.
-# 18. Desenvolva um programa que peça ao usuário para inserir um valor booleano e, em seguida, inverta esse valor.
-# 19. Faça um programa que compare se dois números fornecidos pelo usuário são iguais.
-# 20. Escreva um programa que verifique se dois números fornecidos pelo usuário são diferentes.
-
-# #### try-except e if
-
-# 21: Conversor de Temperatura
-# 22: Verificador de Palíndromo
-# 23: Calculadora Simples
-# 24: Classificador de Números
-# 25: Conversão de Tipo com Validação
+print(f"The square of the value is {square_number}. \n")
