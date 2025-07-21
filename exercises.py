f57d4c7a7c99c949e4ab19bf0a0f59766c602e66
Leane Brunelle <leanebrunelle2@gmail.com>
2025-07-21 16:59:42 -0300

    (ADD) Exercise 2

--- exercises.py
+++ exercises.py
@@ -1,13 +1,24 @@
 # #### Inteiros (`int`)
 
-# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-number_one = int(input("Type a number: "))
-number_two = int(input("Type another number: "))
-number_total = number_one + number_two
+# 1. Write a program that adds two integers entered by the user. 
+# (Escreva um programa que soma dois números inteiros inseridos pelo usuário.)
 
-print(f"The total value is: {number_total}")
+# number_one = int(input("Type a number: "))
+#number_two = int(input("Type another number: "))
+# number_total = number_one + number_two
 
-# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+# print(f"The total value is: {number_total} \n")
+
+# 2. Create a program that receives a number from the user and calculates the remainder of dividing that number by 5.
+# (Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.)
+
+# divider = int(input("Type a number: "))
+# dividend = 5
+# division_total = divider / dividend
+# remainder = divider % dividend
+
+# print(f"The value of division is {division_total} and the remainder is {remainder} \n")
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
