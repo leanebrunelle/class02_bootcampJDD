4be1a1a9c5c281c5153d5ebcfde2d7c45ac0614c
Leane Brunelle <leanebrunelle2@gmail.com>
2025-07-26 10:20:35 -0300

    Exercise 13

--- strings_exercises.py
+++ strings_exercises.py
@@ -11,11 +11,18 @@
 # 12. Create a program that takes the user's full name and prints the name in all lowercase letters.
 # Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 
-full_name = input("Enter your full name: ")
-lower_name = full_name.lower()
+# full_name = input("Enter your full name: ")
+# lower_name = full_name.lower()
 
-print(f"{lower_name} \n")
+# print(f"{lower_name} \n")
 
-# 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+# 13. Develop a program that asks to user enter a sentence and then print the sentence without leading and trailing spaces
+# Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
+
+sentence = input("Enter any sentence: ")
+strip_sentence = sentence.strip()
+
+print(f"{strip_sentence} \n")
+
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
