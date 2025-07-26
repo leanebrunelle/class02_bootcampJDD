b1453ef67b737e68e7ea886fc500b6fac4b2cb80
Leane Brunelle <leanebrunelle2@gmail.com>
2025-07-26 10:01:54 -0300

    Exercise 12

--- strings_exercises.py
+++ strings_exercises.py
@@ -3,12 +3,19 @@
 # 11. Write a program that receives a string from the user and converts it to uppercase.
 # Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 
-name = input("Type your name: ")
-upper_name = name.upper()
+# name = input("Type your name: ")
+# upper_name = name.upper()
 
-print(f"{upper_name} \n")
+# print(f"{upper_name} \n")
 
-# 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+# 12. Create a program that takes the user's full name and prints the name in all lowercase letters.
+# Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+full_name = input("Enter your full name: ")
+lower_name = full_name.lower()
+
+print(f"{lower_name} \n")
+
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
