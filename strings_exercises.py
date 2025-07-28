847c33c0929aec6974b2b54a841fdef8c5a53fca
Leane Brunelle <leanebrunelle2@gmail.com>
2025-07-28 18:09:30 -0300

    Full strings exercises

--- strings_exercises.py
+++ strings_exercises.py
@@ -3,39 +3,39 @@
 # 11. Write a program that receives a string from the user and converts it to uppercase.
 # Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 
-# name = input("Type your name: ")
-# upper_name = name.upper()
+name = input("Type your name: ")
+upper_name = name.upper()
 
-# print(f"{upper_name} \n")
+print(f"{upper_name} \n")
 
 # 12. Create a program that takes the user's full name and prints the name in all lowercase letters.
 # Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 
-# full_name = input("Enter your full name: ")
-# lower_name = full_name.lower()
+full_name = input("Enter your full name: ")
+lower_name = full_name.lower()
 
-# print(f"{lower_name} \n")
+print(f"{lower_name} \n")
 
 # 13. Develop a program that asks to user enter a sentence and then print the sentence without leading and trailing spaces
 # Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 
-# sentence = input("Enter any sentence: ")
-# strip_sentence = sentence.strip()
+sentence = input("Enter any sentence: ")
+strip_sentence = sentence.strip()
 
-# print(f"{strip_sentence} \n")
+print(f"{strip_sentence} \n")
 
 # 14. Write a program that asks to user to enter a date in the "mm/dd/aaaa" format and then print month, day and year separately
 # Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 
-# date = input("Enter your birthday (mm/dd/aaaa): ")
-# split_date = date.split("/")
+date = input("Enter your birthday (mm/dd/aaaa): ")
+split_date = date.split("/")
 
-# print(f" Month {split_date[0, 1]} \n day {split_date[2,3]} \n year {split_date[4: ]} \n")
+print(f" Month: {split_date[0]} \n Day: {split_date[1]} \n Year: {split_date[2]} \n")
 
 # 15. Write a program that concatenates two user-supplied strings
 # # Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
-# string_supp = input("Enter two words: ")
-# concat_word = string_supp.join(-)
+first_inp = input(" Enter your first name: ")
+second_inp = input(" Enter your last name: ")
 
-# print(concat_word)
+print( first_inp + " " + second_inp)
